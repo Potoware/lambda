a6fadc1693eaeac3c069ff4da756207ee4a2d4e3
Alejandro Potosi <alejandropotosi@outlook.com>
2021-09-07 07:52:08 -0500

    Expresiones Lambda, Consumer y Supplier

--- src/com/potoware/lambda/EjemploConsumer.java
+++ src/com/potoware/lambda/EjemploConsumer.java
@@ -1,7 +1,13 @@
 package com.potoware.lambda;
 
+import com.potoware.lambda.models.Usuario;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 public class EjemploConsumer {
     public static void main(String[] args) {
@@ -10,10 +16,25 @@ public class EjemploConsumer {
 
     consumidor.accept(new String());
 
-        BiConsumer<String, Integer> consumerBi = (nombre,edad)->{
-            System.out.println("Hola, tengo "+ edad+" y me llamo "+nombre);
-        };
+        BiConsumer<String, Integer> consumerBi = (nombre,edad)-> System.out.println("Hola, tengo "+ edad+" y me llamo "+nombre);
+
         consumerBi.accept("Pepe",23);
 
+        Consumer<String> consumer2 = System.out::println;
+        consumer2.accept("Hola mundo lambda");
+
+        List<String> nombres = Arrays.asList("Alejo", "Juan", "Daniel", "Nathaly","Carol");
+        nombres.forEach(consumer2);
+
+        Supplier<Usuario> crearUsuario = Usuario::new;
+        Usuario usuario = new Usuario();
+        BiConsumer<Usuario, String> asignarNombre = (a,b)->a.setNombre(b);
+
+        asignarNombre.accept(usuario,"Alejandro");
+        System.out.println(usuario.getNombre());
+
+        Supplier<String> proveedor = ()-> "hola mundo proveedor";
+
+        System.out.println(proveedor.get());
     }
 }
---
+++ src/com/potoware/lambda/models/Usuario.java
@@ -0,0 +1,13 @@
+package com.potoware.lambda.models;
+
+public class Usuario {
+    private String nombre;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
